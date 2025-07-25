07663766a3562cde38e3fe89cb3fa270314cac6a
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-07-25 14:46:34 -0400

    Id emitted on click

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, computed } from '@angular/core';
+import { Component, Output, input, output, computed, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,19 +9,26 @@ import { Component, input, computed } from '@angular/core';
 })
 export class UserComponent {
   // Standard State Management:
+  // @Input({required: true}) id!: string;
   // @Input({required: true}) avatar!: string;
   // @Input({required: true}) name!: string;
- 
+  // @Output() select = new EventEmitter();
+
   // get imagePath() {
   //   return 'assets/users/' + this.avatar
   // }
   
   // With signals:
+  id = input.required<string>();
   avatar = input.required<string>();
   name = input.required<string>();
+  select = output<string>()
+  
   imagePath = computed(() => {
     return 'assets/users/' + this.avatar()
   })
   
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id())
+  }
 }
