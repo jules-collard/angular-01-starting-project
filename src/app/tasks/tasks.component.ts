4dd2e8fe778262a9c83054d8825a7e2affa4683f
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2025-07-25 18:43:39 -0400

    Dynamic task lists

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,21 @@
 import { Component, input, computed } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+
+import { dummyTasks } from "../dummy-tasks";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  name = input<string | undefined>()
+  userId = input<string>();
+  name = input<string>();
+  tasks = dummyTasks
+
+  selectedUserTasks = computed(() => 
+    this.tasks.filter((task) => task.userId === this.userId())
+  )
 }
