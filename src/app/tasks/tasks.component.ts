6c783da98191c7f5ba1e179a4d1e7a4118e9604f
jules-collard <jules_collard@yahoo.co.uk>
2025-07-26 17:24:14 -0400

    Add task dialogue open/close

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -4,18 +4,20 @@ import { TaskComponent } from "./task/task.component";
 import { Task } from "./task/task.model"
 
 import { dummyTasks } from "../dummy-tasks";
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  userId = input<string>();
+  userId = input.required<string>();
   name = input<string>();
   tasks = signal<Task[]>(dummyTasks)
+  isAddingTask = signal<boolean>(false)
 
   selectedUserTasks = computed(() => 
     this.tasks().filter((task) => task.userId === this.userId())
@@ -25,6 +27,14 @@ export class TasksComponent {
     this.tasks.update(
       tasks => tasks.filter(task => task.id !== id)
     )
+  }
+
+  onStartAddTask() {
+    this.isAddingTask.set(true)
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask.set(false)
   }
 }
 
