33c9037a8e84914787c8b9f4cc7c8ad0c9cfef04
jules-collard <jules_collard@yahoo.co.uk>
2025-07-27 11:56:20 -0400

    Local storage for tasks

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -5,6 +5,14 @@ import { NewTaskData, Task } from "./task/task.model";
 @Injectable({providedIn: 'root' })
 export class TasksService {
     tasks = signal<Task[]>(dummyTasks);
+    
+    constructor() {
+        const tasks = localStorage.getItem('tasks')
+
+        if (tasks) {
+            this.tasks.update(() => JSON.parse(tasks))
+        }
+    }
 
     addTask(taskData: NewTaskData, userId: string) {
         this.tasks.update(tasks => [...tasks, {
@@ -14,11 +22,17 @@ export class TasksService {
             summary: taskData.summary,
             dueDate: taskData.date 
         }]);
+        this.saveTasks()
     }
 
     removeTask(id: string) {
         this.tasks.update(
             tasks => tasks.filter(task => task.id !== id)
         );
+        this.saveTasks()
+    }
+
+    private saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks()))
     }
 }
